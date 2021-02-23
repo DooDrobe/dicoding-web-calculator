1dd47ecc3c207d97dc963f1a61a7201efcd95500
doodrobe <hatta616@gmail.com>
2021-02-23 23:03:00 +0700

    Added browser obejt example in test.js

--- test/test.js
+++ test/test.js
@@ -112,3 +112,26 @@ Bonjour Harry!
 //Perlu kita perhatikan bahwa, ketika kita lupa menuliskan 
 //keyword let, const atau var pada script ketika 
 //membuat sebuah variabel, maka variabel tersebut akan menjadi global.
+//--------------------------------------------------------------------
+//BROWSER OBJECT
+const firstName = prompt("Siapa nama depanmu?");
+const lastName = prompt("Siapa nama belakangmu?");
+const language = prompt("Bisa berbahasa apa?");
+ 
+const user = {
+   name: {
+       first: firstName,
+       last: lastName,
+   },
+   language: language
+};
+ 
+if (user.language === "English") {
+   alert("Nice to meet you " + user.name.first + " " + user.name.last + "!");
+} else if (user.language === "French") {
+   alert("Ravi de vous rencontrer " + user.name.first + " " + user.name.last + "!");
+} else if (user.language === "Japanese") {
+   alert("Hajimemashite, " + user.name.first + " " + user.name.last + "!");
+} else {
+   alert("Senang bertemu dengan Anda " + user.name.first + " " + user.name.last + "!");
+}
