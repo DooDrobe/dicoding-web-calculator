8e6dd05b8606fafdf9e18c1bbaf0ce7d5092db4d
doodrobe <hatta616@gmail.com>
2021-02-23 22:44:40 +0700

    Added test.JS which is learning amterial for JS

--- assets/calculator.js
+++
@@ -1,5 +0,0 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript pada Website")
-alert("Terimakasih.")
-//komen
-/*komen1
-komen2*/
---
+++ test/test.js
@@ -0,0 +1,114 @@
+console.log("Selamat Anda berhasil menggunakan JavaScript pada Website")
+alert("Terimakasih.")
+//komen
+/*komen1
+komen2*/
+
+"use strick"; //digunakan untuk memaksa penulisan sintaks js dengan baik benar dan aman
+let x = 10;
+let y = 5
+ 
+x += y; // artinya -> x = x + y;
+x -= y; // artinya -> x = x - y;
+x *= y; // artinya -> x = x * y;
+x /= y; // artinya -> x = x / y;
+x %= y; // artinya -> x = x % y;
+
+x++ //post fix, di print dulu baru ditambah
+++x //prefix, ditambah dulu baru di print
+
+console.log(x)
+
+let user = {
+    name: {
+      first: "Harry",
+      last: "Potter",
+    },
+    age: 20, 
+    isMuggle: false,
+    stuff: ["Magic Wind", "Flying Car", "Owl"]
+};
+
+console.log("Hallo, nama saya " + user.name.first + " " + user.name.last);
+console.log("Umur saya " + user.age + " tahun");
+
+let myArray = ["Coklat", 42.5, 22, true, "Programming"];
+console.log(myArray[1]);
+
+//perbandingan
+const aString = '10';
+const aNumber = 10
+
+console.log(aString == aNumber) // true, karena nilainya sama-sama 10
+console.log(aString === aNumber) // false, karena walaupun nilainya sama, tetapi tipe datanya berbeda
+
+/* output
+true
+false
+*/
+
+
+//for of
+const myArray = ["Harry", "Ron", "Hermione", "Tom"];
+
+for(let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+/* output
+Harry
+Ron
+Hermione
+Tom
+*/
+
+//for of
+
+//function
+function greeting(name, language) {
+    if(language === "English") {
+        console.log("Good Morning " + name + "!");
+    } else if (language === "French") {
+        console.log("Bonjour " + name + "!");
+    } else {
+        console.log("Selamat Pagi " + name + "!");
+    }
+}
+
+greeting("Harry", "French");
+
+/* output
+Bonjour Harry!
+*/
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let result = multiply(10, 2)
+console.log(result)
+
+/* output
+20
+*/
+
+function greeting(name, language) {
+    if(language === "English") {
+        return "Good Morning " + name + "!";
+    } else if (language === "French") {
+        return "Bonjour " + name + "!";
+    } else {
+        return "Selamat Pagi " + name + "!";
+    }
+}
+
+let greetingMessage = greeting("Harry", "French");
+console.log(greetingMessage);
+
+/* output
+Bonjour Harry!
+*/
+
+//Perlu kita perhatikan bahwa, ketika kita lupa menuliskan 
+//keyword let, const atau var pada script ketika 
+//membuat sebuah variabel, maka variabel tersebut akan menjadi global.
